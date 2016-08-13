4b004b5d3b28e9dfdb523600aaac4dd3ee88bae6
Jim Popadak <jim.popadak@outlook.com>
2016-08-13 18:39:01 -0400

    Removed other tests with unsed variables

--- test/CoreMatchers.Tests/EqualsTests.cs
+++ test/CoreMatchers.Tests/EqualsTests.cs
@@ -23,47 +23,5 @@ namespace JPopadak.CoreMatchers.Matchers
             //Xunit.Assert.StartsWith("Value cannot be null", ex.Message);
             //Xunit.Assert.Contains("expected", ex.Message);
         }
-
-        [Fact]
-        public void ExAndActWithSameValue_Eq_True()
-        {
-            // Given
-            string expected = "Value";
-            string actual = "Value";
-
-            // When
-            bool equal = true;
-
-            // Then
-            Xunit.Assert.True(equal);
-        }
-
-        [Fact]
-        public void ExCapitalActLowerWithCase_Eq_False()
-        {
-            // Given
-            string expected = "Value";
-            string actual = "value";
-
-            // When
-            bool equal = false;
-
-            // Then
-            Xunit.Assert.False(equal);
-        }
-
-        [Fact]
-        public void Ex_int_Act_int_Same__Eq__True()
-        {
-            // Given
-            int expected = 0;
-            int actual = int.MaxValue;
-
-            // When
-            bool equal = false;
-
-            // Then
-            Xunit.Assert.False(equal);
-        }
     }
 }
