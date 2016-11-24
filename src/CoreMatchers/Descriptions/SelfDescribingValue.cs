fbe1de5cf4070dc93afba735db01d7cd8b39d01f
Jim Popadak <jim.popadak@outlook.com>
2016-11-23 23:10:33 -0500

    Resolves #28 - Create IsEmptyEnumerable Matcher

---
+++ src/CoreMatchers/Descriptions/SelfDescribingValue.cs
@@ -0,0 +1,17 @@
+﻿namespace JPopadak.CoreMatchers.Descriptions
+{
+    public class SelfDescribingValue<T> : IDescribable
+    {
+        private T value;
+
+        public SelfDescribingValue(T value)
+        {
+            this.value = value;
+        }
+
+        public void Describe(IDescription description)
+        {
+            description.AppendValue(value);
+        }
+    }
+}
---
+++ test/StandardMatchers.Tests/Matchers/IsEmptyEnumerableFacts.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using static JPopadak.StandardMatchers.Matchers.Matchers;
+
+namespace JPopadak.CoreMatchers.Matchers
+{
+    public class IsEmptyEnumerableFacts
+    {
+        [Fact]
+        public void NullValue_EmptyEnumerable_IsNullSafe()
+        {
+            // Given
+            IMatcher<object> matcher = EmptyEnumerable<object>();
+
+            // When
+            TestHelper.AssertNullSafe(matcher);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void UnknownTypeValue_EmptyEnumerable_IsTypeSafe()
+        {
+            // Given
+            IMatcher<object> matcher = EmptyEnumerable<object>();
+
+            // When
+            TestHelper.AssertUnknownTypeSafe(matcher);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void EmptyCollection_EmptyEnumerable_ReturnsTrue()
+        {
+            // Given
+            IMatcher<object> matcher = EmptyEnumerable<object>();
+
+            // When
+            TestHelper.AssertMatches(matcher, emptyList());
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void CollectionWithItems_EmptyEnumerable_ReturnsFalse()
+        {
+            // Given
+            IMatcher<object> matcher = EmptyEnumerable<object>();
+
+            // When
+            TestHelper.AssertDoesNotMatch(matcher, populatedList());
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void EmptyEnumerable_ReturnsDescriptionWithSameInstanceWithString()
+        {
+            // Given
+            IMatcher<object> matcher = EmptyEnumerable<object>();
+
+            // When
+            TestHelper.AssertDescription("an empty IEnumerable", matcher);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void StringType_EmptyEnumerable_CompilesSuccessfullyWithSpecificType()
+        {
+            // Given / When / Then
+            IMatcher<IEnumerable<string>> matcher = EmptyEnumerable<string>();
+        }
+
+        private List<object> emptyList()
+        {
+            return new List<object>();
+        }
+
+        private List<object> populatedList()
+        {
+            return new List<object>() {
+                new object(),
+                new object(),
+                new object(),
+                new object(),
+                new object()
+            };
+        }
+    }
+}
