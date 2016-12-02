edacef3f5bb40ae8828c94756e2774c8514d11d2
jpopadak <jim.popadak@outlook.com>
2016-12-02 10:38:11 -0500

    28 create is empty iterable matcher (#50)
    * Resolves #28 - Create IsEmptyEnumerable Matcher
    * Moved IsDictionaryWithSize to StandardMatchers

---
+++ src/CoreMatchers/Descriptions/SelfDescribingValueEnumerable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace JPopadak.CoreMatchers.Descriptions
+{
+    public class SelfDescribingValueEnumerable<T> : IEnumerable<IDescribable>
+    {
+        private readonly List<SelfDescribingValue<T>> values;
+
+        public SelfDescribingValueEnumerable(IEnumerable<T> values)
+        {
+            List<SelfDescribingValue<T>> describingValues = new List<SelfDescribingValue<T>>();
+            foreach (T value in values)
+            {
+                describingValues.Add(new SelfDescribingValue<T>(value));
+            }
+            this.values = describingValues;
+        }
+
+        public IEnumerator<IDescribable> GetEnumerator()
+        {
+            return values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
