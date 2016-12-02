8403ba33885200b849095fdbabe62279d195d9da
Bryan Herman <TimeDelta@users.noreply.github.com>
2016-12-02 11:56:57 -0500

    resolves #33 - create IsEnumerableWithSize matcher (#52)

--- test/StandardMatchers.Tests/Matchers/Collections/IsDictionaryWithSizeFacts.cs
+++ test/StandardMatchers.Tests/Matchers/Collections/IsDictionaryWithSizeFacts.cs
@@ -13,7 +13,7 @@ namespace JPopadak.StandardMatchers.Matchers.Collections
         public void NullValue_IsDictionaryWithSize_IsNullSafe()
         {
             // Given
-            IMatcher<IDictionary<string, string>> matcher = IsDictionaryWithSize<string, string>(1);
+            IMatcher<IDictionary<string, string>> matcher = DictionaryWithSize<string, string>(1);
 
             // When
             TestHelper.AssertNullSafe(matcher);
@@ -95,12 +95,12 @@ namespace JPopadak.StandardMatchers.Matchers.Collections
 
         private IMatcher<IDictionary<string, string>> getMatcher(int size)
         {
-            return IsDictionaryWithSize<string, string>(size);
+            return DictionaryWithSize<string, string>(size);
         }
 
         private IMatcher<IDictionary<string, string>> getMatcher(IMatcher<int> sizeMatcher)
         {
-            return IsDictionaryWithSize<string, string>(sizeMatcher);
+            return DictionaryWithSize<string, string>(sizeMatcher);
         }
 
         private IDictionary<string, string> getEmptyDictionary()
