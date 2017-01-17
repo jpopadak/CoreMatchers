a62acdd6ffc71f5e6a143d9ba8435c121795bb2a
jpopadak <jim.popadak@outlook.com>
2017-01-17 10:51:14 -0500

    Resolves #24 - create is array matcher (#55)
    - Resolves #24 - Create IsArray Matcher
    - Added tests and updated project.json for all projects to include authors and debug configurations.
    - Removed description methods for actual strings.
    - Added comma to list separator
    - Added comma to test for description

---
+++ src/StandardMatchers/Matchers/Collections/IsArray.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Linq;
+using JPopadak.CoreMatchers.Descriptions;
+using JPopadak.CoreMatchers.Matchers;
+
+namespace JPopadak.StandardMatchers.Matchers.Collections
+{
+    public class IsArray<T> : TypeSafeMatcher<T[]>
+    {
+        private readonly IMatcher<T>[] _elementMatchers;
+
+        public IsArray(params IMatcher<T>[] elementMatchers)
+        {
+            _elementMatchers = new IMatcher<T>[elementMatchers.Length];
+            Array.Copy(elementMatchers, _elementMatchers, elementMatchers.Length);
+        }
+
+        protected override bool MatchesSafely(T[] actualItems)
+        {
+            if (_elementMatchers.Length != actualItems.Length)
+            {
+                return false;
+            }
+
+            for (var i = 0; i < _elementMatchers.Length; ++i)
+            {
+                if (_elementMatchers[i].Matches(actualItems[i]))
+                {
+                    // If it matches, continue
+                    continue;
+                }
+
+                // If it doesnt match, write out why
+                return false;
+            }
+
+            // All matched properly
+            return true;
+        }
+
+        protected override void DescribeMismatchSafely(T[] actualItems, IDescription mismatchDescription)
+        {
+            if (_elementMatchers.Length != actualItems.Length)
+            {
+                mismatchDescription.AppendText("array length was ");
+                mismatchDescription.AppendValue(actualItems.Length);
+                return;
+            }
+
+            for (var i = 0; i < _elementMatchers.Length; ++i)
+            {
+                if (_elementMatchers[i].Matches(actualItems[i]))
+                {
+                    // If it matches, continue
+                    continue;
+                }
+
+                // If it doesnt match, write out why
+                mismatchDescription.AppendText("element ");
+                mismatchDescription.AppendValue(i);
+                mismatchDescription.AppendText(" ");
+                _elementMatchers[i].DescribeMismatch(actualItems[i], mismatchDescription);
+                return;
+            }
+        }
+
+        public override void Describe(IDescription description)
+        {
+            description.AppendList(
+                "[",
+                ", ",
+                "]",
+                // ReSharper disable once CoVariantArrayConversion
+                _elementMatchers);
+        }
+    }
+}
---
+++ test/StandardMatchers.Tests/Matchers/Collections/IsArrayFacts.cs
@@ -0,0 +1,164 @@
+﻿using JPopadak.CoreMatchers;
+using JPopadak.CoreMatchers.Descriptions;
+using JPopadak.CoreMatchers.Matchers;
+using Xunit;
+using static JPopadak.CoreMatchers.Matchers.Matchers;
+using static JPopadak.StandardMatchers.Matchers.Matchers;
+
+namespace JPopadak.StandardMatchers.Matchers.Collections
+{
+    public class IsArrayFacts
+    {
+        [Fact]
+        public void NullValue_ArrayEqualTo_IsNullSafe()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"), EqualTo("d"));
+
+            // When
+            TestHelper.AssertNullSafe(matcher);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void UnknownTypeValue_ArrayEqualTo_IsTypeSafe()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"), EqualTo("d"));
+
+            // When
+            TestHelper.AssertUnknownTypeSafe(matcher);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void ExactValuesAsMatcherValues_ArrayEqualTo_ReturnsTrue()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"));
+            string[] values = {"a", "b", "c"};
+
+            // When
+            TestHelper.AssertMatches(matcher, values);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void DifferentSecondEqualToValues_ArrayEqualTo_ReturnsFalse()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"));
+            string[] values = {"a", "c", "c"};
+
+            // When
+            TestHelper.AssertDoesNotMatch(matcher, values);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void SmallerSizedValueArray_ArrayEqualTo_ReturnsFalse()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"));
+            string[] values = {"a", "b"};
+
+            // When
+            TestHelper.AssertDoesNotMatch(matcher, values);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void LargerSizedValueArray_ArrayEqualTo_ReturnsFalse()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"));
+            string[] values = {"a", "b", "c", "d"};
+
+            // When
+            TestHelper.AssertDoesNotMatch(matcher, values);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void SmallerSizedMatcherArray_ArrayEqualTo_ReturnsFalse()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"));
+            string[] values = {"a", "b", "c"};
+
+            // When
+            TestHelper.AssertDoesNotMatch(matcher, values);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void LargerSizedMatcherArray_ArrayEqualTo_ReturnsFalse()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"), EqualTo("c"), EqualTo("d"));
+            string[] values = {"a", "b", "c"};
+
+            // When
+            TestHelper.AssertDoesNotMatch(matcher, values);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void ArrayEqualToAEqualToB_ArrayEqualTo_HasReadableDescription()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"));
+
+            // When
+            TestHelper.AssertDescription("[\"a\", \"b\"]", matcher);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void ArrayEqualToAEqualToB_ArrayEqualTo_HasReadableMismatchDescription()
+        {
+            // Given
+            IMatcher<string[]> matcher = new IsArray<string>(EqualTo("a"), EqualTo("b"));
+            string[] actual = {"a", "c"};
+
+            // When
+            TestHelper.AssertMismatchDescription("element <1> was \"c\"", matcher, actual);
+
+            // Then - No Exception
+        }
+
+        [Fact]
+        public void CustomMatcher_ArrayEqualTo_HasReadableMismatchDescription()
+        {
+            // Given
+            IMatcher<object> matcher = Array(new CustomMatcher(), EqualTo("a"));
+            string[] actual = {"a", "c"};
+
+            // When
+            TestHelper.AssertMismatchDescription("element <0> didn't match", matcher, actual);
+
+            // Then - No Exception
+        }
+
+        private class CustomMatcher : Matcher<string>
+        {
+            public override bool Matches(object item) { return false; }
+
+            public override void Describe(IDescription description) { description.AppendText("c"); }
+
+            public override void DescribeMismatch(object item, IDescription description)
+            {
+                description.AppendText("didn't match");
+            }
+        };
+    }
+}
