305d823e22ec726b83e279d4b7d47eb82ae1c2a1
jpopadak <jim.popadak@outlook.com>
2017-01-25 09:21:27 -0500

    Resolves #3 - Create IsBlankString Matchers (#57)

---
+++ src/StandardMatchers/Matchers/Text/IsBlankString.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.RegularExpressions;
+using JPopadak.CoreMatchers.Descriptions;
+using JPopadak.CoreMatchers.Matchers;
+
+namespace JPopadak.StandardMatchers.Matchers.Text
+{
+    public class IsBlankString : TypeSafeMatcher<string>
+    {
+        private static readonly Regex RegexWhitespace = new Regex(@"^\s*$", RegexOptions.Multiline);
+
+        public IsBlankString()
+        {
+            // Do Nothing
+        }
+
+        public override void Describe(IDescription description)
+        {
+            description.AppendText("a blank string");
+        }
+
+        protected override bool MatchesSafely(string item)
+        {
+            return RegexWhitespace.IsMatch(item);
+        }
+    }
+}
