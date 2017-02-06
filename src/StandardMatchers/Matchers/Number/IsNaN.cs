2bcd566531f2a44fd4da029ab6603b640911f24d
jpopadak <jim.popadak@outlook.com>
2017-02-06 11:34:26 -0500

    Resolves #14 - Create IsNotANumber Matcher (#61)
    * Resolves #14 - Create IsNotANumber Matcher
    * Ignoring test for checking against Infinity String
       - Some consoles display the infinity symbol and some
          display the word infinity.

---
+++ src/StandardMatchers/Matchers/Number/IsNaN.cs
@@ -0,0 +1,24 @@
+﻿using JPopadak.CoreMatchers.Descriptions;
+using JPopadak.CoreMatchers.Matchers;
+
+namespace JPopadak.StandardMatchers.Matchers.Number
+{
+    public class IsNaN : TypeSafeMatcher<double>
+    {
+        protected override bool MatchesSafely(double item)
+        {
+            return double.IsNaN(item);
+        }
+
+        protected override void DescribeMismatchSafely(double item, IDescription mismatchDescription)
+        {
+            mismatchDescription.AppendText("was ");
+            mismatchDescription.AppendValue(item);
+        }
+
+        public override void Describe(IDescription description)
+        {
+            description.AppendText("a double value of NaN");
+        }
+    }
+}
