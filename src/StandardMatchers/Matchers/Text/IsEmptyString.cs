ef3a4b2669e411ee3073500dcb426d494f03d18f
jpopadak <jim.popadak@outlook.com>
2017-02-06 09:30:26 -0500

    Resolves #4 - Create IsEmptyString Matcher (#62)

---
+++ src/StandardMatchers/Matchers/Text/IsEmptyString.cs
@@ -0,0 +1,18 @@
+﻿using JPopadak.CoreMatchers.Descriptions;
+using JPopadak.CoreMatchers.Matchers;
+
+namespace JPopadak.StandardMatchers.Matchers.Text
+{
+    public class IsEmptyString : TypeSafeMatcher<string>
+    {
+        protected override bool MatchesSafely(string item)
+        {
+            return string.Empty.Equals(item);
+        }
+
+        public override void Describe(IDescription description)
+        {
+            description.AppendText("an empty string");
+        }
+    }
+}
