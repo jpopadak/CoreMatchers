8643f50ab31de726dda5197d4ed412f7540cfdcd
Jim Popadak <james.popadak@ygrene.com>
2018-02-17 13:49:38 -0500

    Resolves #29 - Create IsIn Matcher

---
+++ src/StandardMatchers/Matchers/Collection/IsIn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using JPopadak.CoreMatchers.Descriptions;
+using JPopadak.CoreMatchers.Matchers;
+
+namespace JPopadak.StandardMatchers.Matchers.Collections
+{
+    public class IsIn<T> : Matcher<T>
+    {
+        private readonly ICollection<T> _collection;
+
+        public IsIn(ICollection<T> collection)
+        {
+            _collection = collection;
+        }
+
+        public IsIn(params T[] elements)
+        {
+            _collection = new List<T>(elements);
+        }
+
+        public override bool Matches(object actual)
+        {
+            if (actual is T casted)
+            {
+                return _collection.Contains(casted);
+            }
+            return false;
+        }
+        
+        public override void Describe(IDescription description)
+        {
+            description.AppendText("one of ");
+            description.AppendValueList("{", ", ", "}", _collection);
+        }
+    }
+}
